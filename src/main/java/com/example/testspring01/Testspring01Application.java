8ca582e183f9c9bd47db5e1966caf7360916571d
nph17103 <nph17103@gmail.com>
2023-04-21 00:54:40 +0700

    first commit

---
+++ src/main/java/com/example/testspring01/Testspring01Application.java
@@ -0,0 +1,13 @@
+package com.example.testspring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testspring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testspring01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testspring01/Testspring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testspring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
