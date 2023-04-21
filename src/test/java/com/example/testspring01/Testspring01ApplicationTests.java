6c4481946f53d5a6fcfd939656fe0160d3dcda5f
nph17103 <nph17103@gmail.com>
2023-04-21 22:56:09 +0700

    change test

--- src/test/java/com/example/testspring01/Testspring01ApplicationTests.java
+++ src/test/java/com/example/testspring01/Testspring01ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.testspring01;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class Testspring01ApplicationTests {
 
 	@Test
